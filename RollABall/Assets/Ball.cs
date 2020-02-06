d7a053694f48b44558e1de69b64579292dd7a50e
inewhart <inewhart@iu.edu>
2020-02-06 16:44:11 -0500

    Repo

---
+++ RollABall/Assets/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Ball : MonoBehaviour
+{
+    GameObject ball = new GameObject();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            ball.GetComponent
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+    }
+}
