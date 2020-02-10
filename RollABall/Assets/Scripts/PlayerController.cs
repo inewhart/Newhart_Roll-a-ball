38b92ce356cddc2087ea4ffae24549a38211c5c3
inewhart <inewhart@iu.edu>
2020-02-09 20:42:01 -0500

    Working on collectables

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    private int Count;
     public float speed;
     private Rigidbody rigid;
+    public Text countText;
 
     // Start is called before the first frame update
     void Start()
     {
+        Count = 0;
         rigid = GetComponent<Rigidbody>();
+        countText.text = "Count: " + countText.ToString();
     }
-
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            Count++;
+        }
+        
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
