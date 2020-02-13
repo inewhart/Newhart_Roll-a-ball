08af5ccc363a7041b79d4f3c7b899186e149619c
inewhart <inewhart@iu.edu>
2020-02-13 17:09:54 -0500

    Adding lives

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     private int Count;
+    private int lives;
     public float speed;
     private Rigidbody rigid;
     public Text countText;
+    public Text livesText;
     public Text winText; 
+    public GameObject ParticleFX;
 
     // Start is called before the first frame update
     void Start()
     {
         Count = 0;
         rigid = GetComponent<Rigidbody>();
-        
+        lives = 3;
         winText.text = "";
         Debug.Log(Count);
     }
@@ -32,10 +35,16 @@ public class PlayerController : MonoBehaviour
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
+            Instantiate(ParticleFX,other.transform.position,other.transform.rotation);
             other.gameObject.SetActive(false);
             Count++;
         }
-        
+        if(other.gameObject.CompareTag("Wall"))
+        {
+            lives--;
+            this.transform.position = Vector3.zero;
+            this.rigid.velocity = Vector3.zero;
+        }
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -46,5 +55,13 @@ public class PlayerController : MonoBehaviour
         Debug.Log(Count);
         rigid.AddForce(movement*speed);
         setCountText();
+        // if(Input.GetKeyDown("space"))
+        // {
+        //     this.GetComponent<Animation>().GetComponent<Transform>().position = 
+        //     new Vector3(this.GetComponent<Animation>().GetComponent<Transform>().position.x,
+        //     this.GetComponent<Animation>().GetComponent<Transform>().position.y,
+        //     this.GetComponent<Animation>().GetComponent<Transform>().position.z);
+        //     this.GetComponent<Animation>().Play("Jump");
+        // }
     }
 }
