c05e7a1323cb7a70294ce8a67358c9d9b2a7e5b2
inewhart <inewhart@iu.edu>
2020-02-18 21:42:59 -0500

    added comments

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -25,14 +25,18 @@ public class PlayerController : MonoBehaviour
     }
     void setCountText()
     {
+        //changes level if all items on first platform are picked up
         countText.text = "Count: " + score.ToString();
+        //runs coroutine ones to teleport player after first level is clear
         if(score == 16 && stop == false)
         {
             winText.text = "Stand by... \n  Teleporting shortly...";
             StartCoroutine("sceneChange");
             stop = true;
-            winText.text = "";
+            
         }
+        
+        // sets win text if all items are picked up
         if(score >= 32)
         {
             winText.text = "Congratulations! \n   You Win!";
@@ -40,14 +44,15 @@ public class PlayerController : MonoBehaviour
     }
     void setLivesText()
     {
+        //Sets lives text object
         livesText.text = "Lives: " + lives.ToString();
     }
+    //sets delay before teleportation
     IEnumerator sceneChange() 
     {
-        Debug.Log("hi");
         this.GetComponent<Rigidbody>().velocity = Vector3.zero;
         yield return new WaitForSeconds(3);
-        
+        winText.text = "";
         this.transform.position = new Vector3(0,.5f,49);
         
         
@@ -56,11 +61,12 @@ public class PlayerController : MonoBehaviour
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
+            //creates clone of the particle system when pickup is destroyed and plays particle effect
             Instantiate(ParticleFX,other.transform.position,new Quaternion(0,0,0,0)).GetComponent<ParticleSystem>().Play();
-            // ParticleFX.GetComponent<ParticleSystem>().Play();
             other.gameObject.SetActive(false);
             score++;
         }
+        //decreases lives when wall is hit and resets position
         if(other.gameObject.CompareTag("Wall"))
         {
             lives--;
@@ -73,10 +79,10 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
-        // Debug.Log(Data.score);
         rigid.AddForce(movement*speed);
         setCountText();
         setLivesText();
+        //changes scenes when lives reach 0
         if(lives <= 0)
         {
             SceneManager.LoadScene("End");
