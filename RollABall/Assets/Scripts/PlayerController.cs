25e465732c1581f931a3fa577a69f70e8aeaaa1c
inewhart <inewhart@iu.edu>
2020-02-18 20:46:34 -0500

    added scene transition

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -2,66 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public static class Data
+ {
+     public static int score;
+     public static int lives;
+    //  public int getScore()
+    //  {
 
+    //  }
+ }
 public class PlayerController : MonoBehaviour
 {
-    private int Count;
-    private int lives;
+ 
     public float speed;
     private Rigidbody rigid;
     public Text countText;
     public Text livesText;
     public Text winText; 
     public GameObject ParticleFX;
-
+    private GameObject score;
+    private bool stop;
     // Start is called before the first frame update
     void Start()
     {
-        Count = 0;
+        
         rigid = GetComponent<Rigidbody>();
-        lives = 3;
+        Data.lives = 3;
         winText.text = "";
-        Debug.Log(Count);
+        stop = false;
     }
     void setCountText()
     {
-        countText.text = "Count: " + Count.ToString();
-        if(Count >= 16)
+        countText.text = "Count: " + Data.score.ToString();
+        if(Data.score == 16 && stop == false)
         {
-            winText.text = "You Win!";
+            winText.text = "Standy by... \n  Teleporting shortly...";
+            StartCoroutine("sceneChange");
         }
+        if(Data.score >= 32)
+        {
+            winText.text = "Congratulations! \n   You Win!";
+        }
+    }
+    void setLivesText()
+    {
+        livesText.text = "Lives: " + Data.lives.ToString();
+    }
+    IEnumerator sceneChange() 
+    {
+        stop = true;
+        winText.text = "";
+        yield return new WaitForSeconds(3);
+
+        SceneManager.LoadScene("Scene2");
+        
     }
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
-            Instantiate(ParticleFX,other.transform.position,other.transform.rotation);
+            Instantiate(ParticleFX,other.transform.position,other.transform.rotation).GetComponent<ParticleSystem>().Play();
+            // ParticleFX.GetComponent<ParticleSystem>().Play();
             other.gameObject.SetActive(false);
-            Count++;
+            Data.score++;
         }
         if(other.gameObject.CompareTag("Wall"))
         {
-            lives--;
+            Data.lives--;
             this.transform.position = Vector3.zero;
             this.rigid.velocity = Vector3.zero;
         }
     }
-    // Update is called once per frame
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
-        Debug.Log(Count);
+        Debug.Log(Data.score);
         rigid.AddForce(movement*speed);
         setCountText();
-        // if(Input.GetKeyDown("space"))
-        // {
-        //     this.GetComponent<Animation>().GetComponent<Transform>().position = 
-        //     new Vector3(this.GetComponent<Animation>().GetComponent<Transform>().position.x,
-        //     this.GetComponent<Animation>().GetComponent<Transform>().position.y,
-        //     this.GetComponent<Animation>().GetComponent<Transform>().position.z);
-        //     this.GetComponent<Animation>().Play("Jump");
-        // }
+        setLivesText();
+        if(Data.lives <= 0)
+        {
+
+        }
     }
+    
 }
